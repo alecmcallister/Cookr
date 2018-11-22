438e66272458b620b3724f7466a06f6a195f1ac3
Alec McAllister <a.mcallister123@gmail.com>
2018-11-22 15:40:35 -0700

    Oof ba-boof!

---
+++ Cookr/Data/RecipeStep.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookr
+{
+	/// <summary>
+	/// THIS WAS DONE IN 2 MINUTES
+	/// </summary>
+	[Serializable]
+	public class RecipeStep
+	{
+		public string Title { get; set; }
+		public string Content { get; set; }
+
+		public Dictionary<string, string> Tooltips;
+
+		public RecipeStep() : this("", "") { }
+
+		public RecipeStep(string title, string content)
+		{
+			Title = title;
+			Content = content;
+		}
+	}
+}
