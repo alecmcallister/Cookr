ae70ee30d19198c4cbd36bee2ab56b45b91f1df8
Rajat Singh <rajat.singh@ucalgary.ca>
2018-11-25 15:08:08 -0700

    recipe manager: working xml parser

--- Cookr/Logic/RecipeManager.cs
+++ Cookr/Logic/RecipeManager.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Cookr.Logic
 {
@@ -12,14 +14,26 @@ namespace Cookr.Logic
         private static List<RecipeObject> recipes;
 
         public static void LoadRecipes() {
-            /// For now this function just generated a list with 5 recipe objects
-            /// It will be replaced by a XML reader
 
             List<RecipeObject> loadedRecipes = new List<RecipeObject>();
-            loadedRecipes.Add(new RecipeObject("Asparagus Soup", true, new List<string> { "asparagus", "Soup" }));
-            loadedRecipes.Add(new RecipeObject("Lasgna", true, new List<String> { "lasgna", "mild" }));
-            loadedRecipes.Add(new RecipeObject("Chicken Burger", false, new List<String> { "burger", "Chicken" }));
-            loadedRecipes.Add(new RecipeObject("Pepperoni Pizza", true, new List<String> { "pizza", "cheese", "pepperoni" }));
+            //loadedRecipes.Add(new RecipeObject("Asparagus Soup", true, new List<string> { "asparagus", "Soup" }));
+            //loadedRecipes.Add(new RecipeObject("Lasgna", true, new List<String> { "lasgna", "mild" }));
+            //loadedRecipes.Add(new RecipeObject("Chicken Burger", false, new List<String> { "burger", "Chicken" }));
+            //loadedRecipes.Add(new RecipeObject("Pepperoni Pizza", true, new List<String> { "pizza", "cheese", "pepperoni" }));
+
+            string mainPath = ".\\Recipes";
+
+            IEnumerable<string> recipeFiles = Directory.EnumerateFiles(mainPath, "*.xml");
+
+            foreach (string file in recipeFiles)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(RecipeObject));
+                using (FileStream fileStream = new FileStream(file, FileMode.Open))
+                {
+                    RecipeObject result = (RecipeObject)serializer.Deserialize(fileStream);
+                    loadedRecipes.Add(result);
+                }
+            }
 
             recipes = loadedRecipes;
 
--- Cookr/Logic/RecipeObject.cs
+++ Cookr/Logic/RecipeObject.cs
@@ -4,14 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Cookr.Logic
 {
+    [Serializable, XmlRoot("Recipe")]
     public class RecipeObject
     {
+        [XmlElement("Title")]
         public string title { get; set; }
+
+        [XmlElement("PopularToday")]
         public bool popularToday { get; set; }
+
+        [XmlArray("Tags"), XmlArrayItem("tag")]
         public List<string> tags { get; set; }
+
+        public RecipeObject()
+        {
+            // XML parser and Serialization need this parameterless constructor
+        }
         
         public RecipeObject(string _title, bool _popularToday, List<string> _tags)
         {
