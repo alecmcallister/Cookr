3c4d6c7fa0114e3f0d119b4eccc6c643685707ee
Elijah Pennoyer <elijah.pennoyer@gmail.com>
2018-11-26 16:02:10 -0700

    Changes made to the recipe object! caution needs fixing

--- Cookr/Logic/RecipeObject.cs
+++ Cookr/Logic/RecipeObject.cs
@@ -11,25 +11,84 @@ namespace Cookr.Logic
     [Serializable, XmlRoot("Recipe")]
     public class RecipeObject
     {
+
         [XmlElement("Title")]
-        public string title { get; set; }
+        public string Title { get; set; }
 
         [XmlElement("PopularToday")]
-        public bool popularToday { get; set; }
+        public bool PopularToday { get; set; }
 
-        [XmlArray("Tags"), XmlArrayItem("tag")]
-        public List<string> tags { get; set; }
+        [XmlArray("Tags"), XmlArrayItem("Tag")]
+        public List<string> Tags { get; set; }
+
+        [XmlElement("Rating")]
+        double Rating { get; set; }
+
+        [XmlElement("TitleImage")]
+        string TitleImage { get; set; }
+
+        /// <summary>
+        /// The introductory blurb at the start of the recipe, e.g. "This simple but elegant soup is blah blah blah"
+        /// </summary>
+        [XmlElement("RecipeIntroduction")]
+        string RecipeIntroduction { get; set; }
+
+        /// <summary>
+        /// The prep time for the recipe, in minutes.
+        /// </summary>
+        [XmlElement("PrepTime")]
+
+        int PrepTime { get; set; }
+        /// <summary>
+        /// The cook time for the recipe, in minutes.
+        /// </summary>
+        [XmlElement("CookTime")]
+
+        int CookTime { get; set; }
+        /// <summary>
+        /// The total time for the recipe (Prep time + cook time) in minutes.
+        /// </summary>
+        [XmlElement("TotalTime")]
+        int TotalTime { get; set; }
+
+        /// <summary>
+        /// The number of servings (people fed) by the recipe.
+        /// </summary>
+        [XmlElement("NumberOfServings")]
+        int NumberOfServings { get; set; }
+
+        [XmlArray("Ingredients"), XmlArrayItem("Ingredient")]
+        List<string> Ingredients { get; set; }
+
+        [XmlArray("Tools"), XmlArrayItem("Tool")]
+        List<string> Tools { get; set; }
+
+        //TODO - Figure out Tooltips and RecipeSteps objects
+
 
         public RecipeObject()
         {
             // XML parser and Serialization need this parameterless constructor
         }
-        
-        public RecipeObject(string _title, bool _popularToday, List<string> _tags)
+
+        //All objects: Title, PopularToday, Tags, Rating, TitleImage, RecipeIntroduction, PrepTime, CookTime, TotalTime, 
+        //NumberOfServings, Ingredients, Tools
+        public RecipeObject(string _title, bool _popularToday, List<string> _tags, double _rating, string _titleImage, 
+            string _recipeIntroduction, int _prepTime, int _cookTime, int _totalTime, int _numberOfServings, 
+            List<string> _ingredients, List<string> _tools)
         {
-            title = _title;
-            popularToday = _popularToday;
-            tags = _tags;
+            Title = _title;
+            PopularToday = _popularToday;
+            Tags = _tags;
+            Rating = _rating;
+            TitleImage = _titleImage;
+            RecipeIntroduction = _recipeIntroduction;
+            PrepTime = _prepTime;
+            CookTime = _cookTime;
+            TotalTime = _totalTime;
+            NumberOfServings = _numberOfServings;
+            Ingredients = _ingredients;
+            Tools = _tools;
         }
         
     }
--- Cookr/Logic/SearchEngine.cs
+++ Cookr/Logic/SearchEngine.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Cookr.Logic
-{
+namespace Cookr.Logic {
     /// <summary>
     /// This class is a collection of methods required 
     /// </summary>
